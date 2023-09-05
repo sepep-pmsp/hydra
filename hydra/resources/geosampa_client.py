5a4e431305cee23f70bacfc377cc8d88a858711a
brunoazuma <brunoazuma@usp.br>
2023-09-05 17:32:48 -0300

    Adicionado ConfigurableResource para o geosampa

---
+++ hydra/resources/geosampa_client.py
@@ -0,0 +1,11 @@
+from dagster import ConfigurableResource
+
+from utils.geosampa_client import get_client
+
+class GeosampaClient(ConfigurableResource):
+
+    def __init__(self):
+        self.client = get_client()
+
+    def get_feature(self, feature_name:str, **kwargs):
+        return self.client.get_feature(feature_name, **kwargs)
