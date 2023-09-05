a6c19389f694ab6038825bc93c7f73569348c8c4
brunoazuma <brunoazuma@usp.br>
2023-09-05 18:17:53 -0300

    Alterando o atributo client

--- hydra/resources/geosampa_client.py
+++ hydra/resources/geosampa_client.py
@@ -3,10 +3,7 @@ from dagster import ConfigurableResource
 from hydra.utils.geosampa_client import get_client, GeoSampaWfs
 
 class GeosampaClient(ConfigurableResource):
-    client : GeoSampaWfs = None
-
-    def __init__(self):
-        self.client = get_client()
+    client : GeoSampaWfs = get_client()
 
     def get_feature(self, feature_name:str, **kwargs):
         return self.client.get_feature(feature_name, **kwargs)
