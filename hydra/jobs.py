a076637f560b95541746355c0c50f326112adbc0
Bruno Azuma Balzano <brunoazuma@usp.br>
2023-09-19 19:28:05 -0300

    geosampa_job adicionado ao módulo de jobs

--- hydra/jobs.py
+++ hydra/jobs.py
@@ -4,4 +4,13 @@ from dagster import (
 )
 
 
-censo_job = define_asset_job('censo_job', selection=AssetSelection.groups('censo_bronze'))
+# Definições dos jobs
+geosampa_job = define_asset_job(
+    'geosampa_job',
+    selection=AssetSelection.groups('geosampa_bronze', 'geosampa_silver')
+)
+
+censo_job = define_asset_job(
+    'censo_job',
+    selection=AssetSelection.groups('censo_bronze')
+)
