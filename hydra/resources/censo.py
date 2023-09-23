a9dfbaf4cc994bb520ccdf8c3dc7565a4022a56a
brunoazuma <brunoazuma@usp.br>
2023-09-23 01:12:02 -0300

    download_zipfile atualizado

--- hydra/resources/censo.py
+++ hydra/resources/censo.py
@@ -1,12 +1,18 @@
 from dagster import ConfigurableResource
-import requests
+import urllib.request as req
+import ssl
 
 
 class CensoResource(ConfigurableResource):
     URL = 'https://ftp.ibge.gov.br/Censos/Censo_Demografico_2010/Resultados_do_Universo/Agregados_por_Setores_Censitarios/SP_Capital_20190823.zip'
 
     def download_zipfile(self) -> bytes:
-        r = requests.get(self.URL)
-        zip_content = r.content
+        # Cria um context inseguro. O risco é de interceptação do conteúdo,
+        # o que não é um problema no nosso caso
+        ctx = ssl.SSLContext(ssl.PROTOCOL_TLS)
+        ctx.options |= 0x4
+        
+        r = req.urlopen(self.URL, context=ctx)
+        zip_content = r.read()
 
         return zip_content
