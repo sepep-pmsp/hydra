5a3921a5590266fece4f80d15d17253db2216380
brunoazuma <brunoazuma@usp.br>
2023-09-25 19:43:09 -0300

    Merge do setor censitario do geosampa com censo

--- hydra/config/censo.py
+++ hydra/config/censo.py
@@ -19,6 +19,28 @@ class CensoConfig:
         ]
         return files
     
+    @staticmethod
+    def get_columns_for_file(file:str) -> list[str]:
+        columns = []
+        if file == CensoFiles.DOMICILIO_01:
+            columns = [
+                'V001',
+                'V002',
+                'V012',
+                'V013',
+                'V014',
+                'V017',
+                'V018',
+                'V019',
+                'V020',
+                'V021',
+                'V035',
+                'V039',
+                'V040',
+                'V041'
+            ]
+        return columns
+    
 class CensoFiles:
     _SUFFIX = '_SP1'
     BASICO = f'Basico{_SUFFIX}'
