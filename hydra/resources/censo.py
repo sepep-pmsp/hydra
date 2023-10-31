9668244fff4e1841c5e875e7b8fb695e1e29d48f
brunoazuma <41346451+brunoazuma@users.noreply.github.com>
2023-10-30 22:58:07 -0300

    46 alteração na fonte do censo (#48)
    * Caminho do arquivo zip atualizado
    
    * Método simplificado dada atualização do site original

--- hydra/resources/censo.py
+++ hydra/resources/censo.py
@@ -1,18 +1,12 @@
 from dagster import ConfigurableResource
-import urllib.request as req
-import ssl
+import requests
 
 
 class CensoResource(ConfigurableResource):
-    URL = 'https://ftp.ibge.gov.br/Censos/Censo_Demografico_2010/Resultados_do_Universo/Agregados_por_Setores_Censitarios/SP_Capital_20190823.zip'
+    URL:str = 'https://ftp.ibge.gov.br/Censos/Censo_Demografico_2010/Resultados_do_Universo/Agregados_por_Setores_Censitarios/SP_Capital_20231030.zip'
 
     def download_zipfile(self) -> bytes:
-        # Cria um context inseguro. O risco é de interceptação do conteúdo,
-        # o que não é um problema no nosso caso
-        ctx = ssl.SSLContext(ssl.PROTOCOL_TLS)
-        ctx.options |= 0x4
-        
-        r = req.urlopen(self.URL, context=ctx)
-        zip_content = r.read()
+        r = requests.get(self.URL)
+        zip_content = r.content
 
         return zip_content
