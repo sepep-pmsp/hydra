17b287fbbd86fd431092600b1fcb8ffdf298dd1e
Bruno Azuma Balzano <brunoazuma@usp.br>
2024-01-12 17:52:03 +0000

    DAO ajustado para a escrita de parquet compatível com geoparquet

--- hydra/dao.py
+++ hydra/dao.py
@@ -9,6 +9,7 @@ from logging import (
     getLogger,
 )
 
+
 class DuckDBS3():
     '''
     Classe criada para carregar e salvar os GeoDataFrames como arquivos parquet num bucket S3.
@@ -18,22 +19,22 @@ class DuckDBS3():
     '''
 
     def __init__(
-            self,
-            bucket_name: str,
-            access_key: str,
-            secret_key: str,
-            endpoint: str,
-            db_path: str = None,
-            logger: Logger = getLogger()
-        ) -> None:
+        self,
+        bucket_name: str,
+        access_key: str,
+        secret_key: str,
+        endpoint: str,
+        db_path: str = None,
+        logger: Logger = getLogger()
+    ) -> None:
         self.bucket_name = bucket_name
         self.access_key = access_key
         self.secret_key = secret_key
         self.endpoint = endpoint
         self.db_path = db_path if db_path != None else ':memory:'
         self.logger = logger
-        self._connection:DuckDBPyConnection = None
-    
+        self._connection: DuckDBPyConnection = None
+
     @property
     def connection(self) -> DuckDBPyConnection:
         if self._connection == None:
@@ -56,7 +57,7 @@ class DuckDBS3():
         """
         self.connection.query(query)
 
-    def _get_s3_path_for(self, table:str) -> str:
+    def _get_s3_path_for(self, table: str) -> str:
         s3_path = f's3://{self.bucket_name}/dagster/{table}.parquet'
         return s3_path
 
@@ -66,35 +67,70 @@ class DuckDBS3():
     def _get_writing_output_log_message(self, path: str) -> str:
         return f"Writing S3 object at: {path}"
 
-    def save_parquet(self, table_name:str, rel: DuckDBPyRelation, geometry_columns:list[str]=None, **kwargs) -> str:        
-        geometry_columns = geometry_columns if geometry_columns != None else ['geometry']
-        other_columns = [col for col in rel.columns if col not in geometry_columns]
+    def _geometry_columns_save_fix(self, geometry_columns: list[str]) -> str:
+        geometry_columns = list(filter(None, geometry_columns))
+
+        if not len(geometry_columns):
+            return ''
+
+        # O DuckDB ainda não trabalho com arquivos no formato GeopParquet, por isso transformo a geometria
+        # para salvar um parquet "compatível" de acordo com a especificação
+        # em https://github.com/opengeospatial/geoparquet/blob/main/format-specs/compatible-parquet.md
+        geom_as_blob = ', '.join(
+            [f"ST_AsWKB(ST_FlipCoordinates(ST_Transform(ST_GeomFromText({col}), 'EPSG:31983', 'EPSG:4326'))) AS {col}"
+             for col in geometry_columns])
+
+        return f' , {geom_as_blob}'
+
+    def _to_compatible_geoparquet(self, rel: DuckDBPyRelation, geometry_columns: list[str] = None) -> DuckDBPyRelation:
+        geometry_columns = geometry_columns if geometry_columns != None else []
+        other_columns = [
+            col for col in rel.columns if col not in geometry_columns]
         other_columns = ', '.join(other_columns)
 
-        geom_as_blob = ', '.join(
-            [f'ST_AsWKB(ST_GeomFromText({col})) AS {col}' for col in geometry_columns])
-        
+        geom_as_blob = self._geometry_columns_save_fix(geometry_columns)
+
+        self.logger.info(f'{other_columns}{geom_as_blob}')
+        return rel.select(f'{other_columns}{geom_as_blob}')
+
+    def save_parquet(self, table_name: str, rel: DuckDBPyRelation, geometry_columns: list[str] = None, **kwargs) -> str:
+
         s3_path = self._get_s3_path_for(table_name)
 
         self.logger.info(self._get_writing_output_log_message(s3_path))
-        rel.select(f'{other_columns} , {geom_as_blob}').to_parquet(s3_path, **kwargs)
+
+        self._to_compatible_geoparquet(
+            rel, geometry_columns).to_parquet(s3_path, **kwargs)
 
         return s3_path
 
-    def load_parquet(self, table_name: str, geometry_columns:list[str]=None) -> DuckDBPyRelation:
-        geometry_columns = geometry_columns if geometry_columns != None else ['geometry']
+    def _geometry_columns_load_fix(self, geometry_columns: list[str]) -> str:
+        if geometry_columns == None:
+            return ''
 
-        s3_path = self._get_s3_path_for(table_name)
-        self.logger.info(f'Gerando query para o arquivo {s3_path}...')
+        geometry_columns = list(filter(None, geometry_columns))
+
+        if not len(geometry_columns):
+            return ''
 
         geom_exclude = 'EXCLUDE (' + \
             ', '.join([f'{col}' for col in geometry_columns]) + ')'
         geom_as_text = ', '.join(
             [f'ST_AsText(ST_GeomFromWKB({col})) AS {col}' for col in geometry_columns])
         geom_fix = f'{geom_exclude}, {geom_as_text}'
+        return geom_fix
+
+    def load_parquet(self, table_name: str, geometry_columns: list[str] = None) -> DuckDBPyRelation:
+
+        s3_path = self._get_s3_path_for(table_name)
+        self.logger.info(f'Gerando query para o arquivo {s3_path}...')
+
+        geom_fix = self._geometry_columns_load_fix(geometry_columns)
+
         sql_query = f'SELECT * {geom_fix} FROM read_parquet("{s3_path}")'
         self.logger.info('Query gerada:')
-        self.logger.info(sqlparse.format(sql_query, reindent=True, keyword_case='upper'))
+        self.logger.info(sqlparse.format(
+            sql_query, reindent=True, keyword_case='upper'))
 
         gdf = self.connection.sql(sql_query)
         return gdf
