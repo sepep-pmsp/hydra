014a2eeb4b4a1041be4520e6f29f7e08ace084d0
Bruno Azuma Balzano <brunoazuma@usp.br>
2024-01-12 18:25:48 +0000

    Ajustada definição da resource do duckdb

--- hydra/resources/duckdb_s3_resource.py
+++ hydra/resources/duckdb_s3_resource.py
@@ -3,6 +3,7 @@ from dagster import (
     OpExecutionContext,
 )
 from duckdb import DuckDBPyRelation
+from pydantic import PrivateAttr
 from hydra.dao import DuckDBS3
 
 class DuckDBS3Resource(ConfigurableResource):
@@ -12,35 +13,33 @@ class DuckDBS3Resource(ConfigurableResource):
     A princípio, funciona apenas recebendo todas as configurações de acesso (endpoint, access key e secret) nas configurações iniciais.
     Futuramente, pode ser modificado para construir uma sessão com os valores padrão caso não receba os parâmetros.
     '''
+    bucket_name: str
+    access_key: str
+    secret_key: str
+    endpoint: str
+    db_path: str = None
+    _dao:DuckDBS3 = PrivateAttr()
 
-    def __init__(
-            self,
-            bucket_name: str,
-            access_key: str,
-            secret_key: str,
-            endpoint: str,
-            db_path: str = None
-        ) -> None:
-        self.bucket_name = bucket_name
-        self.dao = DuckDBS3(
-            bucket_name=bucket_name,
-            access_key=access_key,
-            secret_key=secret_key,
-            endpoint=endpoint.removeprefix('http://'),
-            db_path=db_path
+    def setup(self) -> None:
+        self._dao = DuckDBS3(
+            bucket_name=self.bucket_name,
+            access_key=self.access_key,
+            secret_key=self.secret_key,
+            endpoint=self.endpoint,
+            db_path=self.db_path
         )
 
     def save_parquet(self, context: OpExecutionContext, rel: DuckDBPyRelation, geometry_columns:list[str]=None, **kwargs) -> str:
         table_name = context.upstream_output.asset_key.to_python_identifier()
         
-        self.dao.logger = context.log
-        s3_path = self.dao.save_parquet(table_name, rel, geometry_columns, **kwargs)
+        self._dao.logger = context.log
+        s3_path = self._dao.save_parquet(table_name, rel, geometry_columns, **kwargs)
 
         return s3_path
 
     def load_parquet(self, context: OpExecutionContext, geometry_columns=None) -> DuckDBPyRelation:
         table_name = context.upstream_output.asset_key.to_python_identifier()
-        self.dao.logger = context.log
+        self._dao.logger = context.log
         
-        rel = self.dao.load_parquet(table_name, geometry_columns)
+        rel = self._dao.load_parquet(table_name, geometry_columns)
         return rel
