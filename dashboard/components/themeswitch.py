00c9905bcbdf7a693b050b005b809956de4ba7f3
mathhmz <rm551843@fiap.com.br>
2024-02-06 14:53:52 -0300

    Atualizando themes switch

--- dashboard/components/themeswitch.py
+++ dashboard/components/themeswitch.py
@@ -1,19 +1,21 @@
 from typing import Any
 from dash_bootstrap_templates import ThemeSwitchAIO
 import dash_bootstrap_components as dbc
-from dash import html
-
+from dash import html, dcc
 class ThemeSwitch:
 
     def __init__(self) -> None:
+        self.class_names = 'theme_switch_div'
         self.first_theme =  "materia"
         self.second_theme = "cyborg"
-        self.first_theme_url = dbc.themes.MATERIA
-        self.second_theme_url = dbc.themes.CYBORG
+        self.first_theme_url = dbc.themes.CERULEAN
+        self.second_theme_url = dbc.themes.DARKLY
 
     def generate_theme_switch(self) -> html.Div :
         theme_switch = ThemeSwitchAIO(aio_id="theme", themes=[self.first_theme_url, self.second_theme_url],)
-        theme_switch_div = html.Div(theme_switch)
+        store_theme_div = html.Div(dcc.Store(id='theme-store', storage_type='memory', data='theme1'))
+
+        theme_switch_div = html.Div(className= self.class_names , children=[theme_switch, store_theme_div])
 
         return theme_switch_div
     
