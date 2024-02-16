e160a33f69f108cc35214d0ed9259d1af588fe67
Bruno Azuma Balzano <brunoazuma@usp.br>
2024-02-16 18:26:32 +0000

    Server exposto para utilização do gunicorn

--- dashboard/app.py
+++ dashboard/app.py
@@ -9,5 +9,7 @@ app = Dash(external_stylesheets=[dbc.themes.MATERIA])
 
 app.layout = Layout.get_layout()
 
+server = app.server
+
 if __name__ == '__main__':
     app.run_server(debug=True, port=7777)
