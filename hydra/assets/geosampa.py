a55bc7879f2633440b838030855be145e7ef9abf
Bruno Azuma Balzano <brunoazuma@usp.br>
2024-04-10 17:23:46 +0000

    Asset ajustado para materializar apenas se o checksum for alterado

--- hydra/assets/geosampa.py
+++ hydra/assets/geosampa.py
@@ -1,5 +1,6 @@
 from dagster import (
     AssetExecutionContext,
+    AssetKey,
     AssetsDefinition,
     Output,
     asset,
@@ -7,6 +8,7 @@ from dagster import (
 
 from ..resources import GeosampaClient
 from ..config import GeosampaConfig
+from ..utils.io.hash import generate_hash_from_obj
 
 
 def __build_raw_asset(name, group_name="geosampa_bronze") -> AssetsDefinition:
@@ -15,6 +17,7 @@ def __build_raw_asset(name, group_name="geosampa_bronze") -> AssetsDefinition:
         group_name=group_name,
         io_manager_key="bronze_io_manager",
         dagster_type=dict,
+        output_required=False,
     )
     def _asset(
         context: AssetExecutionContext,
@@ -27,11 +30,33 @@ def __build_raw_asset(name, group_name="geosampa_bronze") -> AssetsDefinition:
 
         assert camada["type"] == "FeatureCollection"
 
-        return Output(
-            camada,
-            metadata={
-                'núm. features': len(camada['features']),
-            })
+        # Para que o checksum funcione conforme esperado, o timestamp de
+        # consulta dos dados deve ser removido
+        camada_checksum = camada.copy()
+        camada_checksum['timeStamp'] = None
+
+        checksum = generate_hash_from_obj(camada_checksum)
+
+        materialization_event = context.instance.get_latest_materialization_event(
+            AssetKey([name])
+        )
+
+        previous_checksum = None
+        if materialization_event != None:
+            metadata = materialization_event.asset_materialization.metadata
+            if 'SHA256 Hash da camada' in metadata:
+                previous_checksum = metadata['SHA256 Hash da camada'].value
+
+        if previous_checksum != None and checksum != previous_checksum:
+            context.log.info(f"A camada {name} foi alterada e será atualizada agora.")
+            yield Output(
+                camada,
+                metadata={
+                    'núm. features': len(camada['features']),
+                    'SHA256 Hash da camada': checksum,
+                })
+        else:
+            context.log.info(f"A camada {name} não foi alterada desde a última atualização.")
 
     return _asset
 
