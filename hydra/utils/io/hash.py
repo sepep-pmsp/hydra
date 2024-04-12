7f218d8c9658e93e578ac49ebcba5299cd060640
Bruno Azuma Balzano <brunoazuma@usp.br>
2024-04-12 16:25:51 +0000

    Criada função para gerar checksum ignorando alguns atributos das features

--- hydra/utils/io/hash.py
+++ hydra/utils/io/hash.py
@@ -5,6 +5,14 @@ from pickle import dumps
 def generate_hash_from_obj(obj: object) -> str:
     return generate_hash_from_bytes(dumps(obj))
 
+def generate_hash_from_feature_collection(feat_coll: object) -> str:
+    feat_coll_checksum = feat_coll.copy()
+    feat_coll_checksum['timeStamp'] = None
+    for f in feat_coll_checksum['features']:
+        f['id'] = None
+
+    return generate_hash_from_obj(feat_coll_checksum)
+
 def generate_hash_from_bytes(content: bytes) -> str:
     readable_hash = hashlib.sha256(content).hexdigest()
     return readable_hash
