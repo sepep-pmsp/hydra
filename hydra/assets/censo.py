65dca27cd0cc727ad03fffc22689c7b2b9dd4678
brunoazuma <brunoazuma@usp.br>
2024-12-11 20:04:31 +0000

    Refatorados arquivos do censo 2022

--- hydra/assets/censo.py
+++ hydra/assets/censo.py
@@ -11,10 +11,13 @@ from zipfile import ZipFile
 from io import BytesIO, StringIO
 import pandas as pd
 import json
-import itertools
 import geopandas as gpd
 
-from ..config.censo import CensoConfig, CensoFiles
+from ..config.censo import (
+        CensoConfig,
+        CensoFiles,
+        Censo2022Files
+)
 from ..resources import CensoResource
 from ..utils.io.files import generate_file_hash, extract_text_file
 
@@ -47,14 +50,14 @@ def arquivo_zip_censo(
     group_name= 'censo_2022_bronze'
 )
 
-def arquivo_zip_censo_2022(
+def arquivo_zip_censo_2022_basico_BR(
     context: AssetExecutionContext,
     censo_resource: CensoResource
 ) -> bytes:
     
-    context.log.info(f'Baixando o arquivo zip de {censo_resource.URL_CENSO_2022}')
+    context.log.info(f'Baixando o arquivo zip de {censo_resource.BASEURL_CENSO_2022}{Censo2022Files.BASICO}.zip')
 
-    zip_content = censo_resource.download_zipfile(censo_year= "2022")
+    zip_content = censo_resource.download_zipfile(censo_year= "2022", file=Censo2022Files.BASICO)
     zip_hash = generate_file_hash(zip_content)
 
     context.log.info('Arquivo baixado')
@@ -67,32 +70,109 @@ def arquivo_zip_censo_2022(
 
     return zip_content
 
+def _build_raw_asset_2022(name: str, groupname: str = 'censo_2022_bronze') -> AssetsDefinition:
+    @asset(
+        name=name,
+        group_name=groupname,
+        io_manager_key='bronze_io_manager',
+        ins={'arquivo_zip_censo': AssetIn(key=f'arquivo_zip_censo_2022_{name}')},
+        dagster_type=list[str],
+    )
+    def _raw_asset_2022(
+        context: AssetExecutionContext,
+        arquivo_zip_censo: bytes
+    ):
+
+        base_path = 'Agregados_por_setores_'
+        file_format = '.csv'
+        csv_string = extract_text_file(
+            zip_content=arquivo_zip_censo,
+            base_path=base_path,
+            file_name=name,
+            file_format=file_format,
+            logger=context.log
+        )
+
+        n = 5
+        peek = csv_string[:n]
+
+        return Output(
+            csv_string,
+            metadata={
+                'número de linhas': len(csv_string),
+                f'primeiras {n} linhas': '\n'.join(peek)
+            })
+    return _raw_asset_2022
 
 @asset(
-    group_name="censo_2022_bronze",
     io_manager_key='bronze_io_manager',
+    ins={'csv_string': AssetIn(key=Censo2022Files.BASICO)},
+    group_name='censo_2022_bronze',
 )
-def SP_Malha_Preliminar_2022(
+def basico_2022_digest(
     context: AssetExecutionContext,
-    arquivo_zip_censo_2022: bytes
-):
-    context.log.info('Lendo o conteúdo do arquivo')
-    with ZipFile(BytesIO(arquivo_zip_censo_2022), "r") as z:
-        for filename in z.namelist():  
-            print(filename)  
-            with z.open(filename) as f:  
-                json_string = [line.decode('latin1').strip()
-                                for line in f.readlines()]
-                context.log.info(f'Arquivo {filename} lido')
-                peek = json_string[:5]
+    csv_string: list[str]
+) -> pd.DataFrame:
+    context.log.info(f'Carregando o csv {Censo2022Files.BASICO}')
 
+    dtypes = {
+        'CD_SETOR': object,
+        'SITUACAO': object,
+        'CD_SIT': object,
+        'CD_TIPO': object,
+        'AREA_KM2': float,
+        'CD_REGIAO': object,
+        'NM_REGIAO': object,
+        'CD_UF': object,
+        'NM_UF': object,
+        'CD_MUN': object,
+        'NM_MUN': object,
+        'CD_DIST': object,
+        'NM_DIST': object,
+        'CD_SUBDIST': object,
+        'NM_SUBDIST': object,
+        'CD_BAIRRO': object,
+        'NM_BAIRRO': object,
+        'CD_NU': object,
+        'NM_NU': object,
+        'CD_FCU': object,
+        'NM_FCU': object,
+        'CD_AGLOM': object,
+        'NM_AGLOM': object,
+        'CD_RGINT': object,
+        'NM_RGINT': object,
+        'CD_RGI': object,
+        'NM_RGI': object,
+        'CD_CONCURB': object,
+        'NM_CONCURB': object,
+        'v0001': int,
+        'v0002': int,
+        'v0003': int,
+        'v0004': int,
+        'v0005': float,
+        'v0006': float,
+        'v0007': int
+    }
 
-    
-    return Output(json_string, 
-                    metadata={
-                    'número de linhas': len(json_string),
-                    f'primeiras 5 linhas': '\n'.join(peek)
-                    })
+    df = pd.read_csv(
+        StringIO('\n'.join(csv_string)),
+        sep=';',
+        decimal=',',
+        dtype=dtypes
+    )
+
+    df = df[df['CD_MUN']=='3550308']
+
+    n = 10
+
+    context.add_output_metadata(
+        metadata={
+            'registros': df.shape[0],
+            f'amostra de {n} linhas': MetadataValue.md(df.sample(n).to_markdown()),
+        }
+    )
+
+    return df
 
     
         
@@ -129,36 +209,6 @@ def __build_raw_asset(name: str, groupname: str = 'censo_bronze') -> AssetsDefin
                 f'primeiras {n} linhas': '\n'.join(peek)
             })
     return _raw_asset
-
-@asset(
-    io_manager_key='bronze_io_manager',    
-    group_name='censo_2022_bronze',
-)
-def SP_Malha_Preliminar_2022_digested(
-    context: AssetExecutionContext,
-    SP_Malha_Preliminar_2022:list[str]
-) -> gpd.GeoDataFrame:
-    context.log.info(f'Carregando o JSON Censo 2022')
-
-
-    json_str = ''.join(SP_Malha_Preliminar_2022)
-    json_data = json.loads(json_str)
-    
-    df = gpd.GeoDataFrame.from_features(json_data["features"])
-
-
-    n = 10
-
-    context.add_output_metadata(
-        metadata={
-            'registros': df.shape[0],
-            f'amostra de {n} linhas': MetadataValue.md(df.sample(n).to_markdown()),
-        }
-    )
-
-    return df
-
-
 
 @asset(
     io_manager_key='bronze_io_manager',
@@ -274,3 +324,4 @@ def domicilio01_digest(
 globals().update({_asset.get('name'): __build_raw_asset(_asset.get('name'))
                   for _asset in CensoConfig.get_asset_config().get('censo')})
 
+globals().update({Censo2022Files.BASICO: _build_raw_asset_2022(Censo2022Files.BASICO)})
--- hydra/resources/censo.py
+++ hydra/resources/censo.py
@@ -1,14 +1,21 @@
 from dagster import ConfigurableResource
 import requests
 
+from ..config.censo.censo_2022_config import Censo2022Config, Censo2022Files
 
 class CensoResource(ConfigurableResource):
     URL_CENSO_2010:str = 'https://ftp.ibge.gov.br/Censos/Censo_Demografico_2010/Resultados_do_Universo/Agregados_por_Setores_Censitarios/SP_Capital_20231030.zip'
     BASEURL_CENSO_2022:str = 'https://ftp.ibge.gov.br/Censos/Censo_Demografico_2022/Agregados_por_Setores_Censitarios/Agregados_por_Setor_csv/Agregados_por_setores_'
 
-    def download_zipfile(self, censo_year:str = "2010") -> bytes:
+    def download_zipfile(self, censo_year:str = "2010", file:str=None) -> bytes:
         if censo_year == "2010":
-            r = requests.get(self.URL_CENSO_2010)
-            zip_content = r.content
-            return zip_content
+            url = self.URL_CENSO_2010
+        if censo_year == '2022':
+            # TODO: alterar a lógia para lançar uma mensagem de erro ou exceção
+            if file in Censo2022Config._censo_file_list():
+                url = f'{self.BASEURL_CENSO_2022}{file}.zip'
+
+        r = requests.get(url)
+        zip_content = r.content
+        return zip_content
         
