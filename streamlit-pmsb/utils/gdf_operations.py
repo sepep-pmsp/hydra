8aa26cff1b2e62277bea365057cc10af87e85a8a
olivia-s-a <otrabalhoeschool@gmail.com>
2025-01-23 14:41:55 -0300

    Fazer um gdf com as chaves estrangeiras dos intersec

--- streamlit-pmsb/utils/gdf_operations.py
+++ streamlit-pmsb/utils/gdf_operations.py
@@ -3,27 +3,74 @@ import pandas as pd
 from os.path import join
 from os import makedirs
 
+DATA_DATE = "2024_11_26"
+PATH_ORIGINAL= join("data", DATA_DATE)
+SUBPATH_DISTRITO= "03_consumo_distrito"
+SUBPATH_SUBBAC= "03_consumo_subbac"
+SUBPATH_SUBPREF= "03_consumo_subprefeitura"
+SUBPATH_FCU= "pop_fcu"
+
+
 
 # Dados
 def get_dados(dado:str):
     if dado == 'distrito':
-        distrito = gpd.read_file(join("data", "2024_11_26", "03_consumo_distrito"))
+        distrito = gpd.read_file(join(PATH_ORIGINAL, SUBPATH_DISTRITO))
         return distrito
     elif dado == 'subbac':
-        subbac = gpd.read_file(join("data", "2024_11_26", "03_consumo_subbac"))
+        subbac = gpd.read_file(join(PATH_ORIGINAL, SUBPATH_SUBBAC))
         return subbac
     elif dado == 'subpref':
-        subpref = gpd.read_file(join("data", "2024_11_26", "03_consumo_subprefeitura"))
+        subpref = gpd.read_file(join(PATH_ORIGINAL, SUBPATH_SUBPREF))
         return subpref
     elif dado == 'fcu':
-        fcu = gpd.read_file(join("data", "2024_11_26", "pop_fcu"))
+        fcu = gpd.read_file(join(PATH_ORIGINAL, SUBPATH_FCU))
         return fcu
 
-def find_distrito_name(gdf, prefix:str):
+#Save gdf
+def save_gdf(
+    gdf:gpd.GeoDataFrame, 
+    file_name:str,
+    **kwargs) -> None:
+    
+    full_path = join(PATH_ORIGINAL, 'intersecs')
+    if not exists(full_path):
+        makedirs(full_path)
+    
+    gdf.to_file(join(full_path, file_name), **kwargs)
+
+def find_distrito_name(gdf, prefix:str): #apagar
     gdf_columns=gdf.columns
     for column in gdf_columns:
         if column.startswith(prefix):
             if column.endswith('distrit'):
+                return column
+
+def find_subbac_name(gdf, prefix): #apagar
+    if prefix == 'cd_':
+        info = find_gdf_name(gdf, 'identif', prefix)
+    else:
+        if prefix == 'nm_':
+            info = find_gdf_name(gdf, 'bacia_h', prefix)
+
+    return info
+
+
+def find_gdf_name(gdf, gdf_name:str, prefix:str):
+    if gdf_name== 'distrito':
+        gdf_name = 'distrit'
+    else:
+        if gdf_name == 'subbac':
+            if prefix=='cd_':
+                gdf_name='identif'
+            else:
+                if prefix== 'nm_':
+                    gdf_name='bacia_h'
+
+    gdf_columns=gdf.columns
+    for column in gdf_columns:
+        if column.startswith(prefix):
+            if column.endswith(gdf_name):
                 return column
 
 
@@ -37,8 +84,10 @@ def create_gdf_sorted(
     gdf_columns=gdf.columns
     
     if name_gdf == 'subbac':
-        gdf_sorted[['cd_subbac', 'nm_subbac']] = (
-            gdf[['cd_identif', 'nome_bacia_h']]
+        cd_subbac = find_subbac_name(gdf, 'cd_')
+        nm_subbac = find_subbac_name(gdf, 'nm_')
+        gdf_sorted[[cd_subbac, nm_subbac]] = (
+            gdf[[cd_subbac, nm_subbac]]
         )
     
     else:
@@ -72,7 +121,11 @@ def create_gdf_sorted(
 
     return gdf_sorted
 
-def overlay_intersec(gdf_intersec, name_gdf_intersec, gdf_unidade, cd_unidade):
+def overlay_intersec(name_gdf_intersec, gdf_unidade, name_gdf_unidade):
+    
+    gdf_intersec= get_dados(name_gdf_intersec)
+    cd_unidade = find_gdf_name(gdf_unidade, name_gdf_unidade, 'cd_')
+    
     gdf_intersec = create_gdf_sorted(
         gdf_intersec,
         name_gdf_intersec,
